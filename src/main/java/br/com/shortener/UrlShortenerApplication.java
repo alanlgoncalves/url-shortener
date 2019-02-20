1c923becdf0ce59ddf6ae2145f54ca21cf1cd36b
alanlgoncalves <alan.lgoncalves@outlook.com>
2019-02-20 06:37:28 -0300

    Fix timezone dates

--- src/main/java/br/com/shortener/UrlShortenerApplication.java
+++ src/main/java/br/com/shortener/UrlShortenerApplication.java
@@ -3,8 +3,16 @@ package br.com.shortener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class UrlShortenerApplication {
+
+  @PostConstruct
+  void started() {
+    TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(UrlShortenerApplication.class, args);
